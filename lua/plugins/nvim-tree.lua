15686ce6cacee4e74b0ad87b45a190214335b173
Po7ed <3058960438@qq.com>
2024-07-26 10:30:30 +0800

    fix: change the event of nvim-tree from key to cmd

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,13 +3,14 @@ return {
 	version = "*",
 	lazy = true,
 	-- event = "VeryLazy",
-	keys =
-	{
-		{"<leader>ee","<cmd>NvimTreeToggle<cr>",desc="Toggle the Tree",silent=true},
-		{"<leader>et","<cmd>NvimTreeToggle<cr>",desc="Toggle the Tree",silent=true},
-		{"<leader>eo","<cmd>NvimTreeOpen<cr>",desc="Open the Tree",silent=true},
-		{"<leader>ef","<cmd>NvimTreeFocus<cr>",desc="Tree Focus",silent=true}
-	},
+	-- keys =
+	-- {
+	-- 	{"<leader>ee","<cmd>NvimTreeToggle<cr>",desc="Toggle the Tree",silent=true},
+	-- 	{"<leader>et","<cmd>NvimTreeToggle<cr>",desc="Toggle the Tree",silent=true},
+	-- 	{"<leader>eo","<cmd>NvimTreeOpen<cr>",desc="Open the Tree",silent=true},
+	-- 	{"<leader>ef","<cmd>NvimTreeFocus<cr>",desc="Tree Focus",silent=true}
+	-- },
+	cmd={"NvimTreeToggle","NvimTreeOpen","NvimTreeFocus"},
 	dependencies = {
 	  "nvim-tree/nvim-web-devicons",
 	},
